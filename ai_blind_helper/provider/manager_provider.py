d115ef5e78073f8e8d258e5e50c3a66a07afd787
ArthurJoseBona <arthurjosebona@gmail.com>
2026-03-27 09:41:52 -0300

    fix(manager_provider): remove  atribute from provider

--- ai_blind_helper/provider/manager_provider.py
+++ ai_blind_helper/provider/manager_provider.py
@@ -4,7 +4,7 @@ from config import Config
 class ManagerProvider:
     def __init__(self):
         self.camera = CameraSource()
-        self.screen = ScreenSource()
+        # self.screen = ScreenSource()
         self.audio_input = InputAudioManager()
         self.audio_output = OutputAudioManager()
         # self.keyboard = KeyboardManager(Config.KEYBOARD_PATH)
